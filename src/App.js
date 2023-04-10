cb076f97f2dce8f3848d567c9e734f9edfca2a08
yaozheng <gloriousfutureserbia@gmail.com>
2023-04-11 00:06:48 +0200

    remove extra code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 
 import { useState } from 'react';
-import Board from './Board';
+import Square from './Square';
 
 
 function App() {
-  let [history, setHistory] = useState([Array(9).fill(null)]) 
+  let [history, setHistory] = useState([Array(9).fill(null)])
   let [move, setMove] = useState(0)
   let isX = move % 2 == 0 ? "X" : "O"
   let squares = history[move]
+  console.log(history, move, squares)
   function judgeWinner() {
     let array = [[squares[0], squares[3], squares[6]]
       , [squares[1], squares[4], squares[7]]
@@ -22,29 +23,39 @@ function App() {
     }
     return ''
   }
-  function control(grid) {
+  function handleControl(grid) {
     if (judgeWinner() || squares[grid]) return
-    let currentSuqares = squares.slice(0)
+    let currentSuqares = squares.slice()
     currentSuqares[grid] = isX
-    setMove(move + 1)
-    setHistory([...history.slice(0,move+1), currentSuqares])
-    
+    setHistory([...history.slice(0, move + 1), currentSuqares])
+    setMove(move + 1) 
   }
   function jumpTo(index) {
-    setMove(index)  
+    setMove(index)
   }
-  let Lis = history.map((item, index) => {
-    return <button className="list-group-item bg-info text-white cursor-pointer" key={index} onClick={()=>jumpTo(index)}>{index === 0 ? 'let is start' : `move #${index}`}</button>
+  let List = history.map((item, index) => {
+    return <button className="list-group-item bg-info text-white cursor-pointer" key={index} onClick={() => jumpTo(index)}>{index === 0 ? 'let is start' : `move #${index}`}</button>
   })
+  let tip = judgeWinner() ? <span >{judgeWinner()} wins</span> : <span  >it is {isX}'s turn</span>
   return (
-    <div className="container-fluid text-center  bg-primary vh-100"> 
-      <Board squares={squares} isX={isX} onControl={control} onJudge={judgeWinner}>
-      </Board>
-      <div  className="position-fixed end-0 top-50  translate-middle-y list-group  list-group-flush" >
-        {Lis}
+    <div className="container-fluid text-center  bg-primary vh-100">
+      <h1 className="pt-4 font-serif">{tip}</h1>
+      <div className="row w-50 mx-auto mt-5  bg-secondary">
+        <Square value={squares[0]} onControl={() => handleControl(0)}></Square>
+        <Square value={squares[1]} onControl={() => handleControl(1)}></Square>
+        <Square value={squares[2]} onControl={() => handleControl(2)}></Square>
+        <Square value={squares[3]} onControl={() => handleControl(3)}></Square>
+        <Square value={squares[4]} onControl={() => handleControl(4)}></Square>
+        <Square value={squares[5]} onControl={() => handleControl(5)}></Square>
+        <Square value={squares[6]} onControl={() => handleControl(6)}></Square>
+        <Square value={squares[7]} onControl={() => handleControl(7)}></Square>
+        <Square value={squares[8]} onControl={() => handleControl(8)}></Square>
+      </div>
+      <div className="position-fixed end-0 top-50  translate-middle-y list-group  list-group-flush" >
+        {List}
       </div>
     </div>
-  )     
+  )
 }
 
 export default App;
--- src/Square.js
+++ src/Square.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
 export default function Square({ value, onControl }) {  
     return ( 
-                <div className="border border-info col-4 px-5 text-center fs-2" style={{ height: "100px", lineHeight: "100px", cursor: "pointer" }} onClick={onControl}>{value}</div>
+            <div className="border border-info col-4 px-5 text-center fs-2" style={{ height: "100px", lineHeight: "100px", cursor: "pointer" }} onClick={onControl}>{value}</div>
 
          
     )
